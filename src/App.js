b27fe3e1569ff519e601d28718fc64f1ad8bc1d0
Yash Rastogi <rastogiyash29@gmail.com>
2023-06-15 19:05:14 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from './components/navbar/Navbar';
+import QuizCreator from './components/quiz_creator/QuizCreator';
+import QuizList from './components/quiz_list/QuizList';
+import QuizResult from './components/quiz_result/QuizResult';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import SingleQuiz from './components/single_quiz/SingleQuiz';
+import ErrorPage from './components/ErrorPage/ErrorPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <BrowserRouter> 
+        <Navbar/>
+        <Routes>
+        <Route path="/" element={<QuizCreator/>}/>
+        <Route path="/create_quiz" element={<QuizCreator/>}/>
+        <Route path="/show_available_quizes" element={<QuizList/>}/>
+        <Route path="/quiz_results_all" element={<QuizResult/>}/>
+        <Route path="/show_quiz_result" element={<SingleQuiz/>}/>
+        <Route path="/*" element={<ErrorPage/>}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
